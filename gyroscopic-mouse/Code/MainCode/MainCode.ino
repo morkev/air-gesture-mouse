8547a5d74dbf1ff50c92fac4b526b9fbea6134bf
Kevin Mora <83437383+morkev@users.noreply.github.com>
2022-05-21 01:11:25 -0600

    Update MainCode.ino

--- gyroscopic-mouse/Code/MainCode/MainCode.ino
+++ gyroscopic-mouse/Code/MainCode/MainCode.ino
@@ -1,19 +1,13 @@
-
 #include <MPU6050.h>
 #include <helper_3dmath.h>
-
 #include <I2Cdev.h>
-
 #include <Mouse.h>
-
 #include <Wire.h>
-
-
 
 MPU6050 mpu;
 int16_t ax, ay, az, gx, gy, gz;
 int vx, vy;
-int sensibility=10;                  //Change this value to change the sensitivity of the device
+int sensibility = 10;                  //Change this value to change the sensitivity of the device
         
 
 void setup() {
@@ -22,33 +16,32 @@ void setup() {
   mpu.initialize();
   if (!mpu.testConnection()) {       //check connection with the MPU-6050, if there is no connection stop to work
     while (1);
-    }
+  }
 }
 
 void up(){
   Mouse.move(0, -sensibility);
-  }
+}
 void down(){
   Mouse.move(0, sensibility);
-  }
+}
 void left(){
   Mouse.move(-sensibility, 0);
-  }
+}
 void right(){
   Mouse.move(sensibility, 0);
-  }
+}
 
 
 void loop() {
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
   
   if(ax>=5000){                          //calibration line
-    right(); // 15000
+    right();
   }
   if(ax<=-9000){                         //calibration line
     left();
   }
-    
   if(ay<=-8000){                          //calibration line
     up();
   }
@@ -56,7 +49,7 @@ void loop() {
     down();
   }
 
-//uncomment the following lines to set the right click with a sprint up and the left click with a sprint down (Work in progress part)
+// Sets the right click with a sprint up and the left click with a sprint down.
 /*
   if(gy>=20000){                         //calibration line        
     Mouse.click(MOUSE_RIGHT);
